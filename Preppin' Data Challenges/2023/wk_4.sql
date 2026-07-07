1d7c75f52356b7b991018bb39a8cb46054d12444
durkka25 <280074141+durkka25@users.noreply.github.com>
2026-07-07 17:37:54 +0100

    Update wk_4.sql

---
+++ Preppin' Data Challenges/2023/wk_4.sql
@@ -0,0 +1,58 @@
+--Preppin' Data 2023 Week 4
+
+with months as (
+select *
+    ,'January' as Month
+from pd2023_wk04_january
+union all
+select *
+    ,'February' as Month
+from pd2023_wk04_february
+union all
+select *
+    ,'March' as Month
+from pd2023_wk04_march
+union all
+select *
+    ,'April' as Month
+from pd2023_wk04_april
+union all
+select *
+    ,'May' as May
+from pd2023_wk04_january
+union all
+select *
+    ,'June' as Month
+from pd2023_wk04_june
+union all
+select *
+    ,'July' as Month
+from pd2023_wk04_july
+union all
+select *
+    ,'August' as Month
+from pd2023_wk04_august
+union all
+select *
+    ,'September' as Month
+from pd2023_wk04_september
+union all
+select *
+    ,'October' as Month
+from pd2023_wk04_october
+union all
+select *
+    ,'November' as Month
+from pd2023_wk04_november
+union all
+select *
+    ,'December' as Month
+from pd2023_wk04_december
+)
+select *
+    ,to_date(Joining_day||'-'||Month||'-2023', 'DD-MMMM-YYYY') as "Joining Date"
+from months
+pivot(
+MAX("VALUE")
+for Demographic in ('Ethnicity','Account Type','Date of Birth')
+) as headers
